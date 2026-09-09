6202a4f8fbf909f9cd962a5a225a571f72c52fd0
Levon Kirakosyan <noosx.e@gmail.com>
2026-09-10 01:41:16 +0400

    fix(nix): refresh vendorHash so the flake builds again

--- nix/package.nix
+++ nix/package.nix
@@ -38,7 +38,7 @@ buildGoModule {
   inherit version;
 
   src = lib.cleanSource ../.;
-  vendorHash = "sha256-rtwUWbft5XGEbuBCn0OMCn4TS5Ul+UXJNIqNOzXfU+M=";
+  vendorHash = "sha256-d/ENFm9b1DkIir1lz50VVX1pvuQpwPUVlA5XOC7Jj5o=";
 
   nativeBuildInputs = [
     makeWrapper
